e8ec5480abfaab4a60c92c9e2499cab2cf2e9188
Won Takemura <wjamest@pm.me>
2018-05-08 18:25:05 -0500

    Version 4

--- myphptest.php
+++ myphptest.php
@@ -4,11 +4,11 @@
 
 
   <h1>This is my test PHP Application...</h1>
-  
+
   <h2>Powered by Github, AWS CodeDeploy, and AWS Elastic Beanstalk</h2>
 
   <?php
-  echo "Version 2"
+  echo "Version 4"
   ?>
 
 </body>
